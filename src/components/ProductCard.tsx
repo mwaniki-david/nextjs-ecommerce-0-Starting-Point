eac9056a5f1d9f4cfd50f8037d10a3f1e7521c6d
mwaniki-david <111046198+mwaniki-david@users.noreply.github.com>
2024-06-22 12:42:05 +0300

    SAVEE

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,23 +4,23 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface ProductCardProps {
-    product: Product;
+    Product: Product;
 
 }
 
-export default function ProductCard({product}: ProductCardProps){
-    const isNew = Date.now() - new Date(product.createdAt).getTime() < 1000 * 60 * 60 *24 * 7;
+export default function ProductCard({Product}: ProductCardProps){
+    const isNew = Date.now() - new Date(Product.createdAt).getTime() < 1000 * 60 * 60 *24 * 7;
 
 
     return(
         <Link
-         href={"/products/" + product.id}
+         href={"/products/" + Product.id}
         className="card w-full bg-base-100 hover:shadow-xl transition"
         >
             <figure>
                 <Image
-                src={product.imageurl}
-                alt={product.name}
+                src={Product.imageurl}
+                alt={Product.name}
                 width={800}
                 height={400}
                 className="h-48 object-cover"
@@ -29,11 +29,11 @@ export default function ProductCard({product}: ProductCardProps){
             </figure>
             <div className="card-body">
                 <h2 className="card-title">
-                    {product.name}
+                    {Product.name}
                 </h2>
                 {isNew && <div className="badge badge-secondary">NEW</div>}
-                <p>{product.description}</p>
-                <Pricetag price={product.price}/>
+                <p>{Product.description}</p>
+                <Pricetag price={Product.price}/>
             </div>
         </Link>
     )
